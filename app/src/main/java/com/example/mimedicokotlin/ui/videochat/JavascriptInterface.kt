cf51bf11413f99e5691cd240c882a96930ba6887
Cesar Aldair Garcia Velez <aldairgarciavelez1@gmail.com>
2023-03-20 12:45:24 -0600

    Elimados archivos innecesarios

--- app/src/main/java/com/example/mimedicokotlin/ui/videochat/JavascriptInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mimedicokotlin.ui.videochat
-
-class JavascriptInterface(private val videochatFragment: VideochatFragment) {
-    @android.webkit.JavascriptInterface
-    fun onPeerConnected(){
-        videochatFragment.onPeerConnected()
-    }
-}
--- app/src/main/java/com/example/mimedicokotlin/ui/videochat/VideochatData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mimedicokotlin.ui.videochat
-
-data class VideochatData (
-    val uniqueId: String?,
-    val timestamp: Long
-)
